86480da4f602d6f3ba194a295c9c848cc1663a35
robbfreitas <robertocompromissos@gmail.com>
2022-12-19 18:29:56 -0300

    Add project files.

---
+++ Estudante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeAluguel
+{
+    internal class Estudante
+    {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", "
+                + Email;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SistemaDeAluguel
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Estudante[] vect = new Estudante[10];
+
+            Console.WriteLine("Quantos quartos serão alugados? ");
+            Console.WriteLine("Somente opções de  0 - 9");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Alguel #{i}:");
+                Console.Write("Nome do estudante: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email do estudante: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vect[quarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
